9c617c025f9055415b8944832665b3541ffa8af8
mona-mp <monamp1998@gmail.com>
2022-10-02 19:52:04 +0330

    add getdetail func

--- cmd/getDetail.go
+++ cmd/getDetail.go
@@ -5,6 +5,11 @@ Copyright © 2022 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
+
 	"github.com/spf13/cobra"
 )
 
@@ -18,9 +23,9 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	fmt.Println("getDetail called")
-	// },
+	Run: func(cmd *cobra.Command, args []string) {
+		getDetail(args)
+	},
 }
 
 func init() {
@@ -35,4 +40,31 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// getDetailCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func getDetail(args []string) {
+	args2 := strings.Join(args, "")
+	req, err := http.NewRequest(
+		http.MethodGet,
+		"https://napi.arvancloud.com/vod/2.0/videos/"+args2,
+		nil,
+	)
+
+	if err != nil {
+		log.Fatalf("error creating HTTP request: %v", err)
+	}
+
+	req.Header.Add("Authorization", "Apikey XXXX")
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("error sending HTTP request: %v", err)
+	}
+	defer res.Body.Close()
+	responseBytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("error reading HTTP response body: %v", err)
+	}
+
+	log.Println("We got the response:", string(responseBytes))
 }
